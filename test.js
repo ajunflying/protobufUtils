104e9f5e53bb3dff4a4c295bc98fdefdac19f2f4
sunjunping <sunjunping@gfxiong.com>
2015-09-24 22:44:57 +0800

    测试

--- test.js
+++ test.js
@@ -4,3 +4,20 @@ var buffer = index.encode({a:1,b:3,c:"茶水费",d:{c:1,b:2}});
 console.log(buffer);
 var model = index.decode(buffer);
 console.log(model);
+
+var VChecker = require('v_checker');
+
+
+var obj = {
+	str: "111asf",
+	num: 1000,
+	date: 1231231231,
+	ary: [],
+	json: {
+		a:1
+	}
+};
+var vchecker = new VChecker(obj);
+
+vchecker.requireExist("str");
+console.log(vchecker.error);
